022ad4981e3042933fefe916cdd0cda04e60399f
eliseucostaalbano <56443711+eliseucostaalbano@users.noreply.github.com>
2024-08-14 16:46:09 -0300

    adicionando validação na rotas produtos

--- src/routers/produto-Router.js
+++ src/routers/produto-Router.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const produtoMiddleware = require('../middlewares/produto-Middleware')
 const produtoController = require("../controllers/produto-controller");
+const validateMiddleware = require("../middlewares/validador-middleware")
 
-router.get('/produtos', produtoController.getAllProdutos)
-router.get('/produtos/:id', produtoMiddleware.middleWareGetProdutosBYId, produtoController.getProdutosById)
-router.post('/produtos', produtoMiddleware.insertProdutosMiddlleWare, produtoController.InsertProdutos)
-router.put('/produtos/:id', produtoMiddleware.middlewareUpadateProdutos, produtoController.updateProdutos)
-router.delete('/produtos/:id', produtoMiddleware.middlewareDeleteProdutos, produtoController.deleteProdutos)
+router.get('/produtos',validateMiddleware.validateTokenMiddleware, produtoController.getAllProdutos)
+router.get('/produtos/:id' ,validateMiddleware.validateTokenMiddleware, produtoMiddleware.middleWareGetProdutosBYId, produtoController.getProdutosById)
+router.post('/produtos' ,validateMiddleware.validateTokenMiddleware, produtoMiddleware.insertProdutosMiddlleWare, produtoController.InsertProdutos)
+router.put('/produtos/:id' ,validateMiddleware.validateTokenMiddleware, produtoMiddleware.middlewareUpadateProdutos, produtoController.updateProdutos)
+router.delete('/produtos/:id' ,validateMiddleware.validateTokenMiddleware, produtoMiddleware.middlewareDeleteProdutos, produtoController.deleteProdutos)
 
 module.exports = router
