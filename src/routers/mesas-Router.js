d53eefb4d0c54e1df010188a542243e40ca0c095
eliseucostaalbano <56443711+eliseucostaalbano@users.noreply.github.com>
2024-08-16 15:06:19 -0300

    criando rotas

--- src/routers/mesas-Router.js
+++ src/routers/mesas-Router.js
@@ -6,4 +6,9 @@ const mesasController = require('../controllers/mesas-controller')
 const validadorMiddleware = require('../middlewares/validador-middleware')
 
 router.get('/mesas', mesasController.getAllMesas)
-//router.put("/mesas/:id", middlewareUpdateStatus, controllerUpdateStatus)
+router.get('/mesas/:id', mesasMiddleware.middleWareGetMesasBYId, mesasController.getMesasById)
+router.post('/mesas' , mesasMiddleware.insertMesasMiddlleWare , mesasController.insertMesas)
+router.put('/mesas/:id',mesasMiddleware.middlewareUpadateMesas, mesasController.updateMesas)
+router.delete('')
+
+module.exports = router
