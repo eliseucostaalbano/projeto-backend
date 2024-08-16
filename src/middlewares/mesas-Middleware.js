132e55618895d10874a4ccdb32a3137cbed45cb7
eliseucostaalbano <56443711+eliseucostaalbano@users.noreply.github.com>
2024-08-16 14:05:52 -0300

    crinado o Crud mesas

---
+++ src/middlewares/mesas-Middleware.js
@@ -0,0 +1,52 @@
+
+const produtoModel = require('../models/produto-Model')
+
+async function insertProdutosMiddlleWare(req, res, next){
+    const { nome, preco, categoria} = req.body;
+
+    if(!nome || !preco || !categoria) {
+        return res.status(400).send("Dados Inválidos")
+    }
+
+    next();
+}
+
+async function middleWareGetProdutosBYId(req, res, next) {
+    const { id } = req.params;
+   
+    if (!id) {
+        return res.status(400).send("Dados não encontrados")
+    }
+
+    next()
+}
+
+async function middlewareUpadateProdutos(req, res, next) {
+    const { id } = req.params
+    const { nome, preco, categoria } = req.body
+
+    if (!id || !nome || !preco || !categoria) {
+        return res.status(400).send("Dados incompletos")
+    }
+
+    const produto = await produtoModel.getProdutosByIdModel(id)
+    if (!produto) {
+        res.status(404).send("Produto não encontrado")
+    }
+
+    next()
+}
+
+async function middlewareDeleteProdutos(req, res, next){
+   const { id } = req.params
+   const produto = await produtoModel.getProdutosByIdModel(id)
+
+   if(!id){
+    return res.status(400).send("Dados Invalidos")
+   }
+
+    if(!produto){
+        return res.status(404).send("Produto não encontrado ou já deletado")
+    }
+   next()
+}
---
+++ src/routers/mesas-Router.js
@@ -0,0 +1,1 @@
+//router.put("/mesas/:id", middlewareUpdateStatus, controllerUpdateStatus)
