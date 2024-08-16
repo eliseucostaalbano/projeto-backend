31e22bc955139d1e0506724ad57419eec6fdc092
Carlosgit <lionbllack31@gmail.com>
2024-08-16 13:24:39 -0300

    j

--- src/controllers/categorias-controller.js
+++ src/controllers/categorias-controller.js
@@ -1,41 +1,69 @@
 const categoriasModels = require('../models/categorias-Models')
 
 async function getAllCategorias(req, res){
-    const categorias = await categoriasModels.getAllCategoriasModel()
-    return res.send(categorias);
+
+    try {
+        const categorias = await categoriasModels.getAllCategoriasModel()
+        
+    } catch (error) {
+        
+        return res.send(categorias);
+    }
 }
 
 async function getCategoriaById(req, res) {
     const {id} = req.params;
-    const categorias = await categoriasModels.getCategoriaByIdModel(id)
-
+    try {
+        const categorias = await categoriasModels.getCategoriaByIdModel(id)
+        
+    } catch (error) {
     return res.send(categorias);
+        
+    }
+
 
 }
 
 async function insertNovaCategoria (req, res) {
 
     const {nome} = req.body;
-    await categoriasModels.insertNovaCategoriaModel(nome)
+    try {
+        await categoriasModels.insertNovaCategoriaModel(nome)
+        
+    } catch (error) {
+        return res.status(201).send ("Categoria inserida com sucesso");
+        
+    }
 
-    return res.status(201).send ("Categoria inserida com sucesso");
     
 }
 
 async function updateCategoria(req, res) {
     const {nome} = req.params;
 
-    await categoriasModels.updateCategoriaModel(nome);
-
+    try {
+        await categoriasModels.updateCategoriaModel(nome);
+        
+    } catch (error) {
     return res.send("Categoria atualizada com sucesso")
+        
+    }
+
+
     
 }
 
 async function deleteCategoria(req, res) {
     const{id} = req.params;
-    await categoriasModels.deleteCategoriaModel(id)
 
-    return res.send("Categoria deletada")
+    try {
+        await categoriasModels.deleteCategoriaModel(id)
+        
+    } catch (error) {
+        return res.send("Categoria deletada")
+        
+    }
+
     
 }
 
