8665e63ceb5c6e87e64d549c033207c835d3f9a6
eliseucostaalbano <56443711+eliseucostaalbano@users.noreply.github.com>
2024-08-16 15:56:33 -0300

    as rotas

--- src/routers/mesas-Router.js
+++ src/routers/mesas-Router.js
@@ -5,10 +5,10 @@ const mesasMiddleware = require('../middlewares/mesas-Middleware')
 const mesasController = require('../controllers/mesas-controller')
 const validadorMiddleware = require('../middlewares/validador-middleware')
 
-router.get('/mesas', mesasController.getAllMesas)
-router.get('/mesas/:id', mesasMiddleware.middleWareGetMesasBYId, mesasController.getMesasById)
-router.post('/mesas' , mesasMiddleware.insertMesasMiddlleWare , mesasController.insertMesas)
-router.put('/mesas/:id',mesasMiddleware.middlewareUpadateMesas, mesasController.updateMesas)
-router.delete('')
+router.get('/mesas',validadorMiddleware.validateTokenMiddleware ,mesasController.getAllMesas)
+router.get('/mesas/:id',validadorMiddleware.validateTokenMiddleware ,mesasMiddleware.middleWareGetMesasBYId, mesasController.getMesasById)
+router.post('/mesas',validadorMiddleware.validateTokenMiddleware ,mesasMiddleware.insertMesasMiddlleWare , mesasController.insertMesas)
+router.put('/mesas/:id',validadorMiddleware.validateTokenMiddleware ,mesasMiddleware.middlewareUpadateMesas, mesasController.updateMesas)
+router.delete('/mesas/:id',validadorMiddleware.validateTokenMiddleware ,mesasMiddleware.middlewareDeleteMesas , mesasController.deleteMesas)
 
 module.exports = router
