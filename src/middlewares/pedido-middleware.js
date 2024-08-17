83335b2715320c46bb3e5cef920d8f1db2962c0c
Daniel Santos <dsaraujo718@gmail.com>
2024-08-17 18:16:16 -0300

    Resolvendo tudo

--- src/middlewares/pedido-middleware.js
+++ src/middlewares/pedido-middleware.js
@@ -1,3 +1,5 @@
+const pedidoModel = require("../models/pedido-Models");
+
 async function criarPedidoMiddleware(req, res, next){
     const {id_usuario, id_mesa} = req.body;
 
@@ -5,9 +7,47 @@ async function criarPedidoMiddleware(req, res, next){
         return res.status(404).send("Dados incorretos");
     }
 
+    const {status} = await pedidoModel.statusPorId(id_mesa)
+
+    if(status){
+        return res.status(400).send("Mesa ocupada!")
+    }
+
     next();
 }
 
+async function inserirItemPedidoMiddleware(req, res,next) {
+    const {quantidade, id_mesa, id_produto} = req.body;
+
+    if(!quantidade, !id_mesa, !id_produto){
+        return res.status(404).send("Dados incorretos");
+    }
+
+    next();
+}
+
+async function deletaItemPedidoMiddleware(req,res, next) {
+    const { id } = req.params;
+    const itemProduto = await pedidoModel.deletaItemPedidoModel(id)
+    if(!id) {
+        return res.status(404).send("dado inválido!");
+    }
+
+    if(!itemProduto){
+        return res.status(400).send("Item não encontrado ou já deletado!");
+    }
+
+    next();
+}
+
+async function listarPedidos(params) {
+    
+}
+
+
+
 module.exports = {
-    criarPedidoMiddleware
+    criarPedidoMiddleware,
+    inserirItemPedidoMiddleware,
+    deletaItemPedidoMiddleware
 }
--- src/models/pedido-Models.js
+++ src/models/pedido-Models.js
@@ -15,11 +15,47 @@ async function atualizaStatusMesa(id){
         SET status = true
         WHERE id = ${id}   
     `)
-
     return;
+}
+
+async function  statusPorId(id) {
+    const status = await connection.query(`
+       SELECT status from mesas
+        WHERE id = ${id}
+    `)
+
+    return status.rows[0];
+}
+
+async function inserirItemPedidoModel(quantidade, id_mesa, id_produto) {
+    await connection.query(`
+        INSERT INTO itenspedidos(quantidade, id_mesa, id_produto)
+        VALUES (${quantidade}, ${id_mesa}, ${id_produto})
+    `)
+    return;
+}
+
+async function deletaItemPedidoModel(id) {
+    await connection.query(`
+        DELETE FROM itenspedidos WHERE id = ${id}
+    `)
+    return;
+}
+
+async function listarPedidosModel() {
+    const pedidos = await connection.query(`
+        SELECT * FROM pedidos
+        ORDER By criado_em DESC;
+    `)
+
+    return pedidos.rows[0];
 }
 
 module.exports = {
     criarPedidoModel,
-    atualizaStatusMesa
+    atualizaStatusMesa,
+    statusPorId,
+    inserirItemPedidoModel,
+    deletaItemPedidoModel,
+    listarPedidosModel
 }
